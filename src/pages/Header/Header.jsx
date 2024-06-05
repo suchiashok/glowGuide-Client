9e052b34654773918ca6b41518f139691c9f8e0e
suchi ashok <suchiashok.97@gmail.com>
2024-06-05 00:07:37 -0400

    updating landing page and header

---
+++ src/pages/Header/Header.jsx
@@ -0,0 +1,31 @@
+import "./Header.scss";
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__navbar">
+        <NavLink to="/" className="header__navOptionLink">
+          <div className="header__navOptionEl">
+            <img
+              className="navbar__navOption"
+              src="src/assets/icons/home.png"
+              alt="home"
+            ></img>
+          </div>
+        </NavLink>
+        <NavLink to="/wishlist" className="header__navOptionLink">
+          <div className="header__navOptionEl">
+            <img
+              className="navbar__navOption"
+              src="src/assets/icons/heart.png"
+              alt="wishlist"
+            ></img>
+          </div>
+        </NavLink>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
 import "./HomePage.scss";
+import Header from "../Header/Header";
 
 function HomePage() {
-    return (
-      <>
-      </>
-    )
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <img
+          className="home_hero"
+          src="src/assets/images/hero.jpg"
+          alt="hero"
+        ></img>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
