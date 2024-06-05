d4976de368d66d375f858050b0b82b27c1cab444
suchi ashok <suchiashok.97@gmail.com>
2024-06-05 14:22:30 -0400

    updating styles for header and homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,18 @@
 import "./HomePage.scss";
 import Header from "../Header/Header";
+import SwiperSlider from "../../components/SwiperSlider/SwiperSlider"
 
 function HomePage() {
   return (
     <>
       <Header />
-      <div className="home">
+      <div className="home overlay">
         <img
           className="home_hero"
           src="src/assets/images/hero.jpg"
           alt="hero"
         ></img>
+        <SwiperSlider />
       </div>
     </>
   );
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,29 +1,26 @@
 import "./LandingPage.scss";
 import Button from "@mui/material/Button";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
     <div className="landing">
       <div className="landing__bg">
-      </div>
-      <div className="landing__bg2">
-      <h3 className="landing__logo">glow Guide</h3>
-        <Link to='/home'>
-        <Button
-          className="landing__button"
-          variant="outlined"
-          sx={{
-            color: "#020852",
-            border: "2px solid #020852",
-            position: "absolute",
-            right: "2rem",
-            bottom: "4rem",
-            fontSize: "1rem",
-          }}
-        >
-          Get Started!
-        </Button>
+        <Link to="/home">
+          <Button
+            className="landing__button"
+            variant="outlined"
+            sx={{
+              color: "#020852",
+              border: "2px solid #020852",
+              position: "absolute",
+              right: "2rem",
+              bottom: "4rem",
+              fontSize: "1rem",
+            }}
+          >
+            Get Started!
+          </Button>
         </Link>
       </div>
     </div>
